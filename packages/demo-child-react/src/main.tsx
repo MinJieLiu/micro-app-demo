24e6a6f4f38ea613de8f135ce4e91891c1d3a2c1
MinJieLiu <liumingyi_1@163.com>
2022-01-25 22:03:10 +0800

    refactor: 去除 withReactMicroApp 使用方式

--- packages/demo-child-react/src/main.tsx
+++ packages/demo-child-react/src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { withReactMicroApp } from '@micro-web/sub';
+import { defineMicroApp } from '@micro-web/sub';
+import App, { AppProps } from './App';
 import './index.css';
-import App from './App';
 
 // 单独开发环境
 if (!import.meta.url.includes('microAppEnv')) {
@@ -14,4 +14,15 @@ if (!import.meta.url.includes('microAppEnv')) {
   );
 }
 
-export default withReactMicroApp(App);
+export default defineMicroApp((container) => {
+  function render(props: AppProps) {
+    ReactDOM.render(<App {...props} />, container);
+  }
+  return {
+    mount: render,
+    render: render,
+    unmount() {
+      ReactDOM.unmountComponentAtNode(container);
+    },
+  };
+});
